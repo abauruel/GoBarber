426470f8343f226b10333932d7354511c687801c
abauruel <abauruel@gmail.com>
2020-02-29 11:42:22 +0000

    Added Config Reactotron

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './config/ReactotronConfig';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return <View />;
+}
